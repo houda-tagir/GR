e04e581d7ce499d9b80cb43c411effb11ee9e3c5
houda-tagir <157700045+houda-tagir@users.noreply.github.com>
2024-05-27 13:44:10 +0100

    Add files via upload

---
+++ Gestion-Ressources/src/main/java/org/hospital/modetravail/repository/ModeTravailRepository.java
@@ -0,0 +1,8 @@
+package org.hospital.modetravail.repository;
+
+import org.hospital.modetravail.entities.ModeTravail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ModeTravailRepository extends JpaRepository<ModeTravail,Long> {
+
+}
---
+++ Gestion-Ressources/src/main/java/org/hospital/modetravail/service/ModeTravailService.java
@@ -0,0 +1,5 @@
+package org.hospital.modetravail.service;
+
+public interface ModeTravailService {
+    public void incrementModeTravail( String semaine, String jour);
+}
