7a3bbb45dc3a54719e753b2f28d38306ad746a45
houda-tagir <htagirtagir@gmail.com>
2024-05-28 21:06:59 +0100

    Your commit message

---
+++ src/main/java/org/hospital/modetravail/ModetravailApplication.java
@@ -0,0 +1,13 @@
+package org.hospital.modetravail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModetravailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModetravailApplication.class, args);
+	}
+
+}
