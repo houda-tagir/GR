50afe02cbb54d5723118d6689799df7f0d7db678
houda-tagir <htagirtagir@gmail.com>
2024-05-28 21:09:34 +0100

    ...

--- src/main/java/org/hospital/modetravail/ModetravailApplication.java
+++ src/main/java/org/hospital/modetravail/ModetravailApplication.java
@@ -1,13 +1,18 @@
 package org.hospital.modetravail;
 
+import org.hospital.modetravail.repository.EmployeRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ModetravailApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ModetravailApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ModetravailApplication.class, args);
+    }
+/*    CommandLineRunner satart(){
+        EmployeRepository.save(null,"Aziz_77","Grutier",)
+    }*/
 
 }
